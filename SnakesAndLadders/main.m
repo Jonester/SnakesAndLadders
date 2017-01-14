7bfe5057610263e537d262ef46ad87dde48ebfe8
Jonester <chris.jones78@gmail.com>
2017-01-14 14:16:59 -0500

    Initial Commit

---
+++ SnakesAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesAndLadders
+//
+//  Created by Chris Jones on 2017-01-14.
+//  Copyright © 2017 Jonescr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
